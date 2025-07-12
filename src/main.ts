96561736d314c95fd4f44a19f665249b2f013a99
Josh Koter <josh.koter@gmail.com>
2025-07-12 14:43:46 +0200

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import Player from "./player";
+import { ResizeWindow, drawStatusText } from "./utils";
+import InputHandler from "./input";
+
+window.addEventListener("load", () => {
+  const canvas = document.querySelector("canvas");
+  const ctx = canvas.getContext("2d");
+  const loading = document.getElementById("loading");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  loading.style.display = "none";
+
+  const player = new Player(canvas.width, canvas.height);
+  const input = new InputHandler();
+
+  let lastTime = 0;
+  function animate(timeStamp) {
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player.update(input.lastKey);
+    player.draw(ctx, deltaTime);
+    drawStatusText(ctx, input, player);
+    requestAnimationFrame(animate);
+  }
+  animate(0);
+
+  ResizeWindow(canvas.width, canvas.height);
+});
